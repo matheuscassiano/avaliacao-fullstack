8803ce9098c657e409988fad721b5107343724e2
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 16:04:54 -0300

    feat: add users module

---
+++ apps/front/src/app/modules/users/users-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+
+export const usersRoutes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+];
