76ba4e41a3b181e9ccfec9e93a8164f4dc3fcf2d
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 17:36:40 -0300

    feat: add shared to users module

--- apps/front/src/app/modules/users/users.module.ts
+++ apps/front/src/app/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersComponent } from './users.component';
 import { RouterModule } from '@angular/router';
 import { usersRoutes } from './users-routing.module';
 import { ListComponent } from './components/list/list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(usersRoutes)],
+  imports: [CommonModule, RouterModule.forChild(usersRoutes), SharedModule],
   declarations: [UsersComponent, ListComponent],
 })
 export class UsersModule {}
