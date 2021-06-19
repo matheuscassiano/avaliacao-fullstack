ad228fcb9023cf2bc499c13ef1e1c7c3340b2b32
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 15:29:27 -0300

    feat: add register component to auth module

--- apps/front/src/app/modules/auth/auth.module.ts
+++ apps/front/src/app/modules/auth/auth.module.ts
@@ -5,14 +5,17 @@ import { RouterModule } from '@angular/router';
 import { authRoutes } from './auth-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { InputMaskModule } from 'ngx-input-mask';
 
 @NgModule({
   imports: [
     CommonModule,
+    InputMaskModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(authRoutes),
   ],
-  declarations: [AuthComponent, LoginComponent],
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
