3e644fc81f03f7bd8269265c3da8250eeb337847
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 20:02:23 -0300

    feat: implemente findOneUserByName on validate component

--- apps/front/src/app/modules/auth/components/validate/validate.component.ts
+++ apps/front/src/app/modules/auth/components/validate/validate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-validate',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./validate.component.scss'],
 })
 export class ValidateComponent implements OnInit {
-  constructor() {}
+  user: any;
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthService,
+    private router: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.router.params.subscribe(({ name }) => {
+      this.authService
+        .findOneUserByName(name)
+        .then((res) => {
+          this.user = res;
+          this.user.knowledge = res.knowledge
+            .map((x: any) => x.name)
+            .join(', ');
+        })
+        .catch(console.log);
+    });
+  }
 }
