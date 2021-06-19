9bf24942d0b89256d4e862e652aab7809fc91127
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 10:38:17 -0300

    feat: add user knowledge to user module

--- apps/api/src/app/modules/user/user.module.ts
+++ apps/api/src/app/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserKnowledge } from './entities/user-knowledge.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserKnowledge])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
