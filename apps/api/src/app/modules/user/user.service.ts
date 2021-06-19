d3a5b34cf9ee25db59cce5424cd2d5f39865e4b2
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 07:51:51 -0300

    feat: add repository to use orm functions

--- apps/api/src/app/modules/user/user.service.ts
+++ apps/api/src/app/modules/user/user.service.ts
@@ -6,7 +6,8 @@ import { User } from './entities/user.entity';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private userRepository: Repository<User>
+    @InjectRepository(User)
+    private userRepository: Repository<User>
   ) {}
   async findAll(): Promise<User[]> {
     try {
