c4a32623b6989303d5a30673a24d9e1725e6da7f
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 10:38:23 -0300

    feat: add user knowledge to user service

--- apps/api/src/app/modules/user/user.service.ts
+++ apps/api/src/app/modules/user/user.service.ts
@@ -3,13 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserCreateDto } from './dto/user-create.dto';
 import { UserUpdateDto } from './dto/user-update.dto';
+import { UserKnowledge } from './entities/user-knowledge.entity';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>
+    private userRepository: Repository<User>,
+
+    @InjectRepository(UserKnowledge)
+    private userKnowledgeRepository: Repository<UserKnowledge>
   ) {}
 
   async findAll(): Promise<User[]> {
@@ -37,7 +41,16 @@ export class UserService {
   async create(body: UserCreateDto): Promise<User> {
     try {
       const entity = Object.assign(new User(), body);
-      return await this.userRepository.save(entity);
+
+      const user = await this.userRepository.save(entity);
+      body.knowledge.map(async (knowledgeId) => {
+        await this.userKnowledgeRepository.save({
+          userId: user.id,
+          knowledgeId,
+        });
+      });
+
+      return await this.userRepository.findOneOrFail(user.id);
     } catch (error) {
       throw new HttpException(
         { message: 'Erro ao cadastrar usuário!' },
