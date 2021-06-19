cc9bc6a75600748753b497b08855f2a95ad6ade7
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 12:48:45 -0300

    feat: add login component methods

---
+++ apps/front/src/app/modules/auth/components/login/login.component.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  login = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    this.authService
+      .login({
+        email: this.login.controls.email.value,
+        password: this.login.controls.password.value,
+      })
+      .then((res) => {
+        localStorage.setItem('token', res.access_token);
+        this.router.navigate(['users/list']);
+      })
+      .catch(console.log);
+  }
+}
