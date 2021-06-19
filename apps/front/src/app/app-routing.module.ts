1d1642dcedc67cd00238d967b59766438c099ef6
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 12:44:19 -0300

    feat: add auth module to app router

---
+++ apps/front/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Guards
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
