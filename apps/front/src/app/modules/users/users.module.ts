bbc8b7260f954f2931fd9639643bf77683a0e5e6
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 17:14:25 -0300

    feat: add list component to users module

--- apps/front/src/app/modules/users/users.module.ts
+++ apps/front/src/app/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { RouterModule } from '@angular/router';
 import { usersRoutes } from './users-routing.module';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(usersRoutes)],
-  declarations: [UsersComponent],
+  declarations: [UsersComponent, ListComponent],
 })
 export class UsersModule {}
