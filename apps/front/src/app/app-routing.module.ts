50d2d394a50d5e314c1bb1cb19f49e33f7b0d7f4
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 16:04:46 -0300

    feat: add AppRouting path to UsersModule

--- apps/front/src/app/app-routing.module.ts
+++ apps/front/src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'registros',
+    loadChildren: () =>
+      import('./modules/users/users.module').then((m) => m.UsersModule),
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
