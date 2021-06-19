26116ede8cc5c4092d539f5b75cc7f95016b6549
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 12:44:46 -0300

    feat: add router and auth module to app module

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -1,11 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
