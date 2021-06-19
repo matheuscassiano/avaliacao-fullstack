78fcea2ddc52606eb4476b959687dc430736429a
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 18:10:42 -0300

    feat: add auth guard to shared modules

--- apps/front/src/app/modules/shared/shared.module.ts
+++ apps/front/src/app/modules/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
 import { LogoutComponent } from './components/logout/logout.component';
+import { AuthGuardService } from './services/auth.guard';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [SharedComponent, LogoutComponent],
-  exports: [LogoutComponent]
+  exports: [LogoutComponent],
+  providers: [AuthGuardService]
 })
 export class SharedModule { }
