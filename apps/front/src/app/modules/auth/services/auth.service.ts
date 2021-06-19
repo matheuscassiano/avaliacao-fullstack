c6aeb6e34b7ceb39f9347108dabebe3879558821
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 15:27:08 -0300

    feat: add to auth service register and findAllKnowledge methods

--- apps/front/src/app/modules/auth/services/auth.service.ts
+++ apps/front/src/app/modules/auth/services/auth.service.ts
@@ -17,4 +17,19 @@ export class AuthService {
   login(user: { email: string; password: string }): Promise<any> {
     return this.httpClient.post(`${this.baseUrl}/auth/login`, user).toPromise();
   }
+
+  findAllKnowledge(): Promise<any> {
+    return this.httpClient.get(`${this.baseUrl}/knowledge`).toPromise();
+  }
+
+  register(user: {
+    name: string;
+    email: string;
+    password: string;
+    cpf: string;
+    phoneNumber: string;
+    knowledge: number[];
+  }): Promise<any> {
+    return this.httpClient.post(`${this.baseUrl}/users`, user).toPromise();
+  }
 }
