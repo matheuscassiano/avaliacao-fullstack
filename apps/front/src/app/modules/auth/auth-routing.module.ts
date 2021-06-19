9f2764e08f60a46c17dc516e5251529f23a86e4c
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 15:29:13 -0300

    feat: add register component to auth router

--- apps/front/src/app/modules/auth/auth-routing.module.ts
+++ apps/front/src/app/modules/auth/auth-routing.module.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 export const authRoutes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: ':name/registrar',
+    component: RegisterComponent,
   },
 ];
