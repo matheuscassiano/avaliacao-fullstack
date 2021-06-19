c80d97545e9a3567fcc5c5f0993a0eab4220c8f6
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 08:42:22 -0300

    feat: create user update dto

---
+++ apps/api/src/app/modules/user/dto/user-update.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class UserUpdateDto {
+  @IsNotEmpty({ message: 'Campo nome é obrigatório!' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Campo email é obrigatório!' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Campo senha é obrigatório!' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Campo CPF é obrigatório!' })
+  @Length(14)
+  cpf: string;
+
+  @IsNotEmpty({ message: 'Campo Telefone é obrigatório!' })
+  phoneNumber: string;
+}
