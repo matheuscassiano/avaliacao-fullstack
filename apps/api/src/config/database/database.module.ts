b3b8f8585b2ab921b85af9542be36cb38b1807aa
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 09:24:50 -0300

    refactor: remove not utilized import

--- apps/api/src/config/database/database.module.ts
+++ apps/api/src/config/database/database.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../../app/modules/user/entities/user.entity';
 import { databaseProviders } from './database.providers';
 
 @Module({
