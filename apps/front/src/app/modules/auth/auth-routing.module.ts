e97b4d07561f9e2302f5495388391f4e01fda012
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 18:26:42 -0300

    feat: add validate component to auth routing

--- apps/front/src/app/modules/auth/auth-routing.module.ts
+++ apps/front/src/app/modules/auth/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ValidateComponent } from './components/validate/validate.component';
 
 export const authRoutes: Routes = [
   {
@@ -10,6 +11,10 @@ export const authRoutes: Routes = [
   {
     path: ':name/registrar',
     component: RegisterComponent,
+  },
+  {
+    path: ':name/validar',
+    component: ValidateComponent,
   },
   {
     path: '**',
