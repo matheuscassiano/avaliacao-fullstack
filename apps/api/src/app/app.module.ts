f093ca66938f3ac911b86bc07ba48029cc6b4d28
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 09:25:59 -0300

    feat: add knowledge module to app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { DatabaseModule } from '../config/database/database.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnowledgeModule } from './modules/knowledge/knowledge.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [KnowledgeModule, UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
