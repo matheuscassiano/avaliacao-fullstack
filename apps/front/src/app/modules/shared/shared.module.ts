d5bdb9de1f576f7dcb9a9f0789ac98f851cd4a7a
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 22:25:18 -0300

    feat: add FeatherModule settings to shared module

--- apps/front/src/app/modules/shared/shared.module.ts
+++ apps/front/src/app/modules/shared/shared.module.ts
@@ -4,10 +4,13 @@ import { SharedComponent } from './shared.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { AuthGuardService } from './services/auth.guard';
 
+import { FeatherModule } from 'angular-feather';
+import icons from './icon.provider';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FeatherModule.pick(icons)],
   declarations: [SharedComponent, LogoutComponent],
-  exports: [LogoutComponent],
+  exports: [LogoutComponent, FeatherModule],
   providers: [AuthGuardService],
 })
 export class SharedModule {}
