eb86e6aced70e9cb31eb026a970908f193c41a1f
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-19 22:10:24 -0300

    refactor: remove not urilized code

--- apps/front/src/app/modules/shared/services/auth.guard.ts
+++ apps/front/src/app/modules/shared/services/auth.guard.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  Router,
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { Router, CanActivate } from '@angular/router';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
   constructor(private _router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
+  canActivate(): boolean {
     const token = localStorage.getItem('token') || null;
-    console.log(token)
     if (token !== null) {
       return true;
     }
